5cecd6f128eb48ce24cd041c3a0a3e98bd643e06
ocowchun <ocowchun@gmail.com>
2014-04-09 12:03:07 +0800

    Enhancement: add toString to List

--- bin/hw2/result.sc
+++ bin/hw2/result.sc
@@ -5,7 +5,6 @@ object result {
   val t1 = new NonEmpty(3, new Empty, new Empty)  //> t1  : hw2.NonEmpty = {.3.}
   val t2 = new NonEmpty(4, t1, new Empty)         //> t2  : hw2.NonEmpty = {{.3.}4.}
 
-  val result1 = List(1, 2, 3)                     //> result1  : hw2.List.List[Int] = hw2.List$Cons@229eb46b
-
-  val result2 = List(t2)                          //> result2  : hw2.List.List[Int] = hw2.List$Cons@2985158
+  val result1 = List(1, "2", 3)                   //> result1  : hw2.List.List[Any] = {1,{2,{3,{}}}}
+val result2 = List(t2)                            //> result2  : hw2.List.List[Int] = {3,{4,{}}}
 }
--- src/hw2/List.scala
+++ src/hw2/List.scala
@@ -5,17 +5,19 @@ object List {
     def isEmpty: Boolean
     def head: T
     def tail: List[T]
+
   }
-  
 
   class Cons[T](val head: T, val tail: List[T]) extends List[T] {
     def isEmpty = false
+  override def toString = "{"+head.toString+","+tail.toString+"}"
   }
 
   class Nil[T] extends List[T] {
     def isEmpty: Boolean = true
     def head: Nothing = throw new NoSuchElementException("Nil head")
     def tail = throw new NoSuchElementException("Nil tail")
+    override def toString = "{}"
   }
 
   def reduce[T](numbers: Array[T]): List[T] = {
@@ -26,12 +28,13 @@ object List {
       new Cons(numbers(0), reduce(numbers.slice(1, length)))
     }
   }
+
   def apply(set1: IntSet) = reduce(set1.toArray)
-  def apply[T:ClassManifest] (numbers: T*)={
-     val ary = new Array[T](numbers.length)
+  def apply[T: ClassManifest](numbers: T*) = {
+    val ary = new Array[T](numbers.length)
     for (i <- 0 until numbers.length) {
-    val elem=numbers(i)
-      ary.update(i,elem)
+      val elem = numbers(i)
+      ary.update(i, elem)
     }
     reduce(ary)
   }
